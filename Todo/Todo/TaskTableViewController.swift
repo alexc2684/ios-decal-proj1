2e647142c7e7ccd1c00717982eb8b7ef5e917c79
Alex Chan <alex.chan@berkeley.edu>
2016-10-19 18:20:55 -0700

    finished

--- Todo/Todo/TaskTableViewController.swift
+++ Todo/Todo/TaskTableViewController.swift
@@ -84,17 +84,14 @@ class TaskTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            taskhelper.itemsList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
